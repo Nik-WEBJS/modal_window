204778473278984fede39d31455bd52df4ccef35
Nik <nikita.fokin0123@gmail.com>
2020-12-07 22:16:59 +0600

    create object and modal

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'Nik modal',
+    closable: true,
+    content: `
+        <h4>Modal is working</h4>
+        <p>Lorem LoremLoremLoremLoremLorem Lorem</p>
+    `,
+    width: '400px',
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler(){
+            console.log('Primary btn click')
+            modal.close()
+        }},
+        {text: 'Cancel', type: 'danger', handler(){
+            console.log('Danger btn click')
+            modal.close()
+        }}
+    ]
+})
